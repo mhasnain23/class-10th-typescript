9c74cc3e7ec2957b383034c02045c1f3ec7577a8
mhasnain23 <tryfrelancer23@gmail.com>
2024-05-14 10:25:46 -0700

    nit

---
+++ app.ts
@@ -0,0 +1,27 @@
+let arrayWithObject = [
+    "My Info",
+    {
+        name: "Hasnain",
+        rollNo: 166548,
+        description: "Love To Do code in Typescript and more",
+    },
+    "Cloud Applied GenAI Progress",
+    {
+        html: true,
+        css: true,
+        tailwind_css: true,
+        javascript: true,
+        typescript: true,
+        react: false,
+    },
+    "Dream Cars Name",
+    { manufracturedByTyota: "Supra", manufracturedByTyota_T: "Lexus RX 330" },
+    "currently learning advanced typescript",
+    {
+        typescriptGenerics: "Generics",
+        typescriptUtilty: "Utilities type",
+        typescriptOOPs: "Object Oriented Programming",
+    },
+];
+
+console.log(arrayWithObject);
